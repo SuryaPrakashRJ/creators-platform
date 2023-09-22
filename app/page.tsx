cc1205674f5607342f0e8211483f1ab587960a9f
Surya <suryaaprakashrj@gmail.com>
2023-09-22 14:03:21 +0530

    added pae

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
-
 import Navbar from "@/components/landing-page/Navbar/Navbar";
-import Hero from "@/components/landing-page/Hero/Hero"
+import Hero from "@/components/landing-page/Hero/Hero";
 export default function page() {
-  
   return (
     <div>
-    <div className="bg-green-950 bg-gradient-to-b from-green-950 via-transparent h-screen">
-      <Navbar/>
-      <Hero/>
+      <div className="bg-green-950 bg-gradient-to-b from-green-950 via-transparent h-screen">
+        <Navbar />
+        <Hero />
+      </div>
     </div>
-    <div>
-    </div>
-    </div>
-  )
+  );
 }
