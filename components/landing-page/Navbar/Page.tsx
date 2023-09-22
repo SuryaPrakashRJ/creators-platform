5ca50788eaf5caaa05e918d74e8588bd107ec351
dinakardhanush <dinakardhanush01@gmail.com>
2023-09-22 14:13:19 +0530

    made changes to landing page

--- components/landing-page/Navbar/Page.tsx
+++ components/landing-page/Navbar/Page.tsx
@@ -61,7 +61,7 @@ const Page = () => {
         <a className="font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="#">Account</a>
         <a className="font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="#">Work</a>
         <a className="font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="#">Blog</a> */}
-            <ul className="justify-center md:items-center space-y-6 flex flex-col md:flex md:flex-row md:space-x-6 md:space-y-0">
+            {/* <ul className="justify-center md:items-center space-y-6 flex flex-col md:flex md:flex-row md:space-x-6 md:space-y-0">
               <Link
                 className="font-semibold text-gray-600 hover:text-green-500"
                 href="#Features"
@@ -80,11 +80,11 @@ const Page = () => {
               >
                 About
               </Link>
-            </ul>
+            </ul> */}
 
             <a
               className="flex items-center gap-x-2 font-semibold text-gray-600 hover:text-green-500 md:border-l md:border-gray-300 md:my-6 md:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-green-500"
-              href="#"
+              href="/sign-up"
             >
               {/* <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
@@ -93,7 +93,7 @@ const Page = () => {
             </a>
             <a
               className="flex items-center  font-semibold text-gray-600 hover:text-green-500  md:border-gray-300 md:my-6 md:pl-2 dark:border-gray-700 dark:text-gray-400 dark:hover:text-green-500"
-              href="#"
+              href="/sign-in"
             >
               {/* <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
